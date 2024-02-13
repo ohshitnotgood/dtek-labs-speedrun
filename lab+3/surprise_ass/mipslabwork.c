3d96e9b8e8bdceb1ba881306eea6f83e8284eaf9
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2024-02-13 16:03:52 +0100

    added starter files for surprise assignment

---
+++ lab+3/surprise_ass/mipslabwork.c
@@ -0,0 +1,60 @@
+/* mipslabwork.c
+
+   This file written 2015 by F Lundevall
+   Updated 2017-04-21 by F Lundevall
+
+   This file should be changed by YOU! So you must
+   add comment(s) here with your name(s) and date(s):
+
+   This file modified 2017-04-31 by Ture Teknolog
+
+   For copyright and licensing, see file COPYING */
+
+#include <stdint.h>  /* Declarations of uint_32 and the like */
+#include <pic32mx.h> /* Declarations of system-specific addresses etc */
+#include "mipslab.h" /* Declatations for these labs */
+
+int prime = 1234567;
+int mytime = 0x5957;
+
+char textstring[] = "text, more text, and even more text!";
+
+int timerCounter = 0;
+/* Interrupt Service Routine */
+void user_isr(void)
+{
+    if (check_t2Timer_timeout()) {
+        // display_string(0, "hello")
+        timerCounter++;
+    }
+
+    if (timerCounter == 10)
+    {
+        time2string(textstring, mytime);
+        display_string(3, textstring);
+        display_update();
+        tick(&mytime);
+        timerCounter = 0;
+    }
+}
+
+/* Lab-specific initialization goes here */
+void labinit(void)
+{
+    // enable interrupt in IEC(0). IEC(0) is T2IE (timer 2 interrupt enable flag)
+    // IPC(2) contains T2IP (timer 2 interrupt priority control)
+    IEC(0) = 0x1 << 8 | IEC(0);
+    /// Set priority
+    IPC(2) = 0b111 << 2 | IPC(2);
+    IPC(2) = 0b11 | IPC(2);
+    init_timer_2();
+    enable_interrupt();
+}
+
+/* This function is called repetitively from the main program */
+void labwork(void)
+{
+    prime = nextprime(prime);
+    display_string(0, itoaconv(prime));
+    display_update();
+}
---
+++ lab+3/surprise_ass/t2.c
@@ -0,0 +1,70 @@
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
+
+/// ## Assignment 2: (b)
+///
+/// 
+/// To initialise the timer, 
+void init_timer_2(void) {
+    disable_t2_timer();
+    clear_t2_timer();
+    set_t2_timer_scaling();
+    start_t2_timer();
+}
+
+/// According to the timers data sheet (page 9, https://ww1.microchip.com/downloads/en/DeviceDoc/61105F.pdf)
+/// setting bit 15 to 1 enables the T2 timer module.
+void enable_t2con(void) {
+    T2CON = 0x1 << 15 | T2CON;
+}
+
+/// According to the timers data sheet (page 9, https://ww1.microchip.com/downloads/en/DeviceDoc/61105F.pdf)
+/// setting bit 15 to 0 disables the T2 timer module.
+void disable_t2_timer(void) {
+    T2CON = 0x0 << 15 | T2CON;
+}
+
+/// Applies 1:8 scaling on the timer. This is done by setting 111 to bits <6::4> in T2CON.
+///
+/// We are using the timer in 16 bit mode 
+///
+/// For this assignment, the clock speed was set to 80MHz. This means every second, 1 million clock cycles are executed.
+/// If we divide this by 1.000, this means it takes 100.000 clock cycles for the timer to timeout.
+///
+/// This would mean after every 100.000 timeouts, one second will have passed.
+void set_t2_timer_scaling(void) {
+    T2CON = 0x70 | T2CON;
+    PR2 = ((80000000 / 256) / 10);
+}
+
+void start_t2_timer(void) {
+    T2CON = 0b1 << 15 | T2CON;
+}
+
+void clear_t2_timer(void) {
+    TMR2 = 0x0;
+}
+
+/// Checks if T2CON timer has timed out.
+///
+/// If a timeout has occurred, the interrupt flag is automatically reset.
+///
+/// @returns 0 if timeout has not been reached.
+/// @returns 1 if timeout HAS been reached.
+int check_t2Timer_timeout(void) {
+    if (IFS(0) & 0x100) {
+        IFSCLR(0) = 0x100;
+        return 1;
+    }
+    return 0;
+}
+
+/// According to the timers data sheet (page 9, https://ww1.microchip.com/downloads/en/DeviceDoc/61105F.pdf)
+/// bits, <6::4> are used for configuring the timer scale.
+///
+///
+/// 
+void configure_timer_preScale() {
+
+}
