2df87dbdf612ff65b967f0f0f639012d0caf958d
Dylan Kreiner <dylanhornkreiner@gmail.com>
2024-02-27 11:23:31 +0100

    factorial update

--- Lab 4/files-lab4/factorial.asm
+++ Lab 4/files-lab4/factorial.asm
@@ -3,8 +3,6 @@
 addi $a0, $zero, 5
 addi $a1, $a0, 0
 addi $a2, $a0, 0
-addi $a3, $a0, 0
-
 
 beq $a0, $zero, zero #if input is 0, go to special label
 
@@ -12,19 +10,19 @@ addi $v0, $a0, 0  #v0 is initially n
 
 
 factorial:
-	addi	$a1, $a1, -1 #decrease a1
-	beq	$a1, 1, end #if a1 is 1, we have multiplied to the end n * n-1 ... * 1
+	addi	$a0, $a0, -1 #decrease a1
+	beq	$a0, 1, end #if a0 is 1, we have multiplied to the end n * n-1 ... * 1
 	
-	add	$a3, $zero, $v0	# Store sum from multiplication
-	add	$a2, $a1, $zero #a2 is n-1
+	add	$a2, $zero, $v0	# Store sum from multiplication
+	add	$a1, $a0, $zero #a1 is n-1
 	
-	multiply: #multiply a2 by a3, result to v0 (a3 is total so far)
-		addi	$a2, $a2, -1 #decrease a2
+	multiply: #multiply a1 by a2, result to v0 (a2 is total so far)
+		addi	$a1, $a1, -1 #decrease a1
 		
-		add	$v0, $v0, $a3 #add a3 to total
+		add	$v0, $v0, $a2 #add a2 to total
 		
 		
-		beq	$a2, 1, factorial # if we have multiplied a2 times, we go to the next multiplication in the factorial
+		beq	$a1, 1, factorial # if we have multiplied a1 times, we go to the next multiplication in the factorial
 		add $0, $0, $0 #nop
 		
 		beq	$zero, $zero, multiply # repeat
